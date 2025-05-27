f2b65ae8895105aa4125f84d6c20490eb673699e
Ian <40207202@yonder.nl>
2025-05-27 12:01:09 +0200

    uuhhh fix

--- xml&json/json.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-header('Content-type: application/json');
-
-$jsonFile = 'emails.json';
-$jsonString = file_get_contents($jsonFile);
-
-var_dump($jsonString);
-?>
